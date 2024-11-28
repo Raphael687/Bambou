ea0cc5ac415ebbde6bb36046f6bf42fe839a8635
Raphael687 <rhuppe@leseminaire.ca>
2024-11-28 19:56:20 +0000

    commentaire

---
+++ histoire.py
@@ -0,0 +1,22 @@
+Question1 = " prenez vous l'épé des ames? Oui (O) ou Non (N)"
+Question2 = " vous êtes mort"
+Question3 = " Vous tourner la tête et un monstre avec des dents assérere apparés, dégainer l'épé? Oui (O) ou Non (N)"
+Question4 = " le monstre vous mange tout crue"
+Question5 = " Le monstre donne un coup de griffe, esquiver ou bloquer l'attaque? Esquive (E) ou Bloquer (B)"
+Question6 = " le bloquage échous, vous mourrez de saignement"
+Question7 = " L'esquive fonctionne, vouler vous donner un coup critique? Oui (O) ou Non (N)"
+Question8 = " Le monstre profite de l'avantage et vous mourer englouti"
+Question9 = " Le coup critique touche le coup, le monstre attaque de nouveau. essayer vous de bloque ou esquiver? Bloquer (B) ou Esquiver (E)"
+Question10 = " L'esquive échou, vous mourer empaler"
+Question11 = " Le Bloquage fonctione, vouler vous achever le dragon? Oui (O) ou Non (N)"
+Question12 = " contrairement a vous le monstre na pas de pitier et vous tue d'un coup"
+Question13 = " Victoire, Merci d'avoir jouer"
+
+
+reponse1 = str(input(Question1)) 
+if reponse1 == "O":
+    reponse2 = input("vous êtes mort.")
+elif reponse1 == "N":
+    reponse3 = str(input(Question3))
+    
+        
